92c42eae692d5acb8259884e18ca8c6e9afdef78
JuanCote <lyy1707@gmail.com>
2023-10-15 11:31:09 +0300

    fix: dotenv error

--- mongo_db.py
+++ mongo_db.py
@@ -1,9 +1,12 @@
 import os
 
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
+load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI")
+print(MONGODB_URI)
 client = MongoClient(MONGODB_URI)
 db = client.dictionary_bot
 
